1a3e3df15decb3838a9fc716add7d07c5e2f9c23
nabi.lee <pinot.kim@kakao.com>
2022-08-22 17:07:54 +0900

    Initial commit with template

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { GetServerSideProps, NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className={"container mx-auto px-4 py-8"}>
+      <h1>Hello SCVSoft!</h1>
+      <p>This is just template project</p>
+    </div>
+  );
+};
+
+export default Home;
